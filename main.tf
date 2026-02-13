f14dd63642ba37afccb9bdc8fc1a66ca74cfa918
Morgan Okanya <morganokanyair@gmail.com>
2026-02-12 20:20:59 -0600

    Initial Terraform infrastructure setup

---
+++ main.tf
@@ -0,0 +1,149 @@
+############################
+# S3 Bucket
+############################
+
+resource "aws_s3_bucket" "app_bucket" {
+  bucket = "${var.project_name}-bucket"
+
+  tags = {
+    Name = "${var.project_name}-bucket"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public" {
+  bucket = aws_s3_bucket.app_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+############################
+# DynamoDB Table
+############################
+
+resource "aws_dynamodb_table" "app_table" {
+  name         = "${var.project_name}-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "${var.project_name}-table"
+  }
+}
+
+###########################
+# IAM Role (Trust Policy)
+############################
+
+resource "aws_iam_role" "ec2_role" {
+  name = "${var.project_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+############################
+# IAM Policy (Permissions)
+############################
+
+resource "aws_iam_policy" "ec2_policy" {
+  name = "${var.project_name}-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "S3List"
+        Effect = "Allow"
+        Action = ["s3:ListBucket"]
+        Resource = aws_s3_bucket.app_bucket.arn
+      },
+      {
+        Sid    = "S3ObjectAccess"
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+        Resource = "${aws_s3_bucket.app_bucket.arn}/*"
+      },
+      {
+        Sid    = "DynamoDBAccess"
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:Scan",
+          "dynamodb:Query",
+          "dynamodb:DescribeTable"
+        ]
+        Resource = aws_dynamodb_table.app_table.arn
+      }
+    ]
+  })
+}
+
+############################
+# Attach Policy to Role
+############################
+
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.ec2_policy.arn
+}
+
+############################
+# Instance Profile
+############################
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "${var.project_name}-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+############################
+# Get Latest Amazon Linux
+############################
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+############################
+# EC2 Instance
+############################
+
+resource "aws_instance" "app_instance" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+
+  tags = {
+    Name = "${var.project_name}-instance"
+  }
+}
